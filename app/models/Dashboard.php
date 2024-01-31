689d18637140baae0bb87397e073d2fa0038940f
kriollo <32275426+kriollo@users.noreply.github.com>
2024-01-31 00:28:05 -0300

    Add Dashboard model and update GlobalControllers

---
+++ app/models/Dashboard.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Models;
+
+use RedBeanPHP\R;
+use versaWYS\kernel\RedBeanCnn;
+
+class Dashboard extends \RedBeanPHP\SimpleModel
+{
+
+    public function getMenuAdmin()
+    {
+        return R::getAll("SELECT vm.seccion,
+        vm.id AS id_menu, vm.nombre AS menu, vm.descripcion AS desc_menu, vm.icono AS ico_menu, vm.url AS url_menu,vm.submenu AS smenu,
+        vsm.id AS id_submenu, vsm.nombre AS submenu, vsm.descripcion AS desc_submenu, vsm.icono AS ico_submenu, vsm.url AS url_submenu
+        FROM versamenu AS vm LEFT JOIN versasubmenu AS vsm ON vm.id = vsm.id_menu AND vsm.estado = 1
+        WHERE vm.estado = 1
+        ORDER BY vm.posicion ASC, vsm.posicion asc");
+    }
+
+    public function __construct()
+    {
+        (new RedBeanCnn())->setup();
+    }
+
+    public function __destruct()
+    {
+        R::close();
+    }
+}
--- versaWYS/kernel/GlobalControllers.php
+++ versaWYS/kernel/GlobalControllers.php
@@ -22,6 +22,8 @@ class Globalcontrollers
         'email' => 'guest@localhost',
     ];
 
+    protected $menu_user = [];
+
 
     /**
      * @var int $id
@@ -38,12 +40,21 @@ class Globalcontrollers
 
     public function __construct($twig, $session)
     {
+
+        global $request;
+
         $this->template = $twig;
         if ($session->get('id_user') !== null) {
             $this->user = (new models\Users())->find($session->get('id_user'));
             $this->id_user = $session->get('id_user');
+
+            if ($this->user['role'] == 'admin') {
+                $this->menu_user = (new models\Dashboard())->getMenuAdmin();
+            } else {
+            }
         }
         $twig->addGlobal('current_user', $this->user);
-
+        $twig->addGlobal('menu_user', $this->menu_user);
+        $twig->addGlobal('url_actual', $request->getUrl());
     }
 }
