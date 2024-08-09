ec53916b66a31d7a18759347d7a37830ca998db8
kriollo <32275426+kriollo@users.noreply.github.com>
2024-08-08 23:26:54 -0400

    feat: update modal component to use Vue 3 composition API

--- src/dashboard/js/components/modal.js
+++ src/dashboard/js/components/modal.js
@@ -1,9 +1,10 @@
 'use strict';
+import { app } from '@/dashboard/js/vue-instancia';
 import { html } from '@/vendor/code-tag/code-tag-esm.js';
 // @ts-ignore
 import { computed, ref, watch } from 'vue';
 
-export const modal = {
+const modal = {
     emits: ['accion'],
     props: {
         idModal: {
@@ -55,11 +56,9 @@ export const modal = {
             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-blur drop-shadow-versaWYS">
             <div class="relative p-4 w-full max-h-full" :class="size">
                 <!-- Modal content -->
-                <div
-                    class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <!-- Modal header -->
-                    <div
-                        class="p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <div class="p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <slot name="modalTitle"></slot>
                     </div>
                     <!-- Modal body -->
@@ -75,4 +74,13 @@ export const modal = {
             </div>
         </div>
     `,
+};
+
+app.component('modal', modal);
+
+export default {
+    name: 'modal',
+    components: {
+        modal,
+    },
 };
