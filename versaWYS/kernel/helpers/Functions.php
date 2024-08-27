c7c93cc1e7bb4c9faebbdd80099341c76a63c2b3
kriollo <32275426+kriollo@users.noreply.github.com>
2024-08-27 17:42:19 -0400

    feat: Agregar función getParamsPaginate para obtener parámetros de paginación

--- versaWYS/kernel/helpers/Functions.php
+++ versaWYS/kernel/helpers/Functions.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace versaWYS\kernel\helpers;
 
+use DateException;
 use DateTime;
 use Exception;
 use IntlDateFormatter;
@@ -325,6 +326,72 @@ class Functions
             $config['assets'][$module][$type][$pathSource] .
             '/' .
             $file;
+    }
+
+    /**
+     * Retrieves the parameters for pagination.
+     *
+     * @param mixed $http The HTTP request object.
+     * @param array $filedsToFilters An array of fields to filter.
+     * @return array The pagination parameters.
+     */
+    public static function getParamsPaginate($http, array $filedsToFilters)
+    {
+        $page = (int) ($http->get('page') ?? 1);
+        $per_page = (float) ($http->get('per_page') ?? 15);
+        $fitro = (string) ($http->get('filter') ?? '');
+        $order = (string) ($http->get('order') ?? 'id,asc');
+        $order = str_replace(',', ' ', $order);
+
+        $externalFilters = (string) ($http->get('externalFilters') ?? '');
+
+        if ($externalFilters != '') {
+            $externalFilters = "{$externalFilters}";
+        } else {
+            $externalFilters = '';
+        }
+
+        if ($page == '' && !is_numeric($page)) {
+            $page = 1;
+        }
+        if ($per_page == '' && !is_numeric($per_page)) {
+            $per_page = 15;
+        }
+
+        if ($page > 1) {
+            $page = (int) ($page - 1) * $per_page;
+        } else {
+            $page = 0;
+        }
+
+        $limit = "LIMIT $page , $per_page";
+
+        $filter = " {$externalFilters} ";
+        if (trim($fitro) != '') {
+            if (trim($filter) != '') {
+                $filter .= ' AND ';
+            }
+
+            $filter .= ' ( ';
+            foreach ($filedsToFilters as $key => $value) {
+                if ($key == 0) {
+                    $filter .= " $value LIKE '%$fitro%' ";
+                } else {
+                    $filter .= " OR $value LIKE '%$fitro%' ";
+                }
+            }
+            $filter .= ' ) ';
+        }
+
+        return [
+            'page' => $page,
+            'per_page' => $per_page,
+            'filtro' => $fitro,
+            'order' => $order,
+            'externalFilters' => $externalFilters,
+            'limit' => $limit,
+            'filter' => $filter,
+        ];
     }
 
     /**
