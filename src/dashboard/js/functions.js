e747dc611cc12d39f0bf6e8c383858dcb037a5fd
kriollo <32275426+kriollo@users.noreply.github.com>
2024-09-06 23:41:29 -0400

    Refactorizar archivo functions.js eliminando código comentado innecesario

--- src/dashboard/js/functions.js
+++ src/dashboard/js/functions.js
@@ -211,26 +211,3 @@ export const VersaToast = Swal.mixin({
     timer: 1500,
     timerProgressBar: true,
 });
-
-// (async () => {
-//     await Toast.fire({
-//         icon: 'success',
-//         title: 'Success',
-//     });
-//     await Toast.fire({
-//         icon: 'error',
-//         title: 'Error',
-//     });
-//     await Toast.fire({
-//         icon: 'warning',
-//         title: 'Warning',
-//     });
-//     await Toast.fire({
-//         icon: 'info',
-//         title: 'Info',
-//     });
-//     await Toast.fire({
-//         icon: 'question',
-//         title: 'Question',
-//     });
-// })();
