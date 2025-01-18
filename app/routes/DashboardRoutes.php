c74a0503f33cb5094b65149359bf2742d2e4811b
kriollo <32275426+kriollo@users.noreply.github.com>
2025-01-17 23:48:12 -0300

    refactoring: actualiza documentación de rutas para usar con modulo /doc

--- app/routes/DashboardRoutes.php
+++ app/routes/DashboardRoutes.php
@@ -6,6 +6,9 @@
  * This file contains the route definitions for the dashboard related endpoints.
  */
 
+//TODO: portal de acceso para usuarios dt.gob.cl
+//TODO: si la IP es de la red interna de la DT (200.72.242.0/27), redirigir a la página de acceso de la DT SIN VALIDAR
+
 declare(strict_types=1);
 
 namespace app\routes;
@@ -16,92 +19,78 @@ use app\middleware\AuthMiddleware;
 use app\middleware\UsersMiddleware;
 use versaWYS\kernel\Router;
 
-// Rutas de ingreso a la raiz del sitio
+// description: Define the root page.
 Router::get('/', [DashBoardController::class, 'index'])->middleware([[AuthMiddleware::class, 'redirectIfSession']]);
 
-/**
- * Define the route for the admin dashboard.
- * This route requires authentication.
- */
+// description: Define the route for the admin dashboard.
 Router::get('/admin', [DashBoardController::class, 'dashboard'])->middleware([[AuthMiddleware::class, 'checkSession']]);
 
-/**
- * Define the route for the admin login page.
- * This route redirects if a session is already active.
- */
+// description: Define the route for the admin login page.
 Router::get('/admin/login', [DashBoardController::class, 'login'])->middleware([
     [AuthMiddleware::class, 'redirectIfSession'],
 ]);
 
-/**
- * Define the route for the authentication process of the admin login.
- * This route requires CSRF token validation, login parameter validation, and login attempts validation.
- */
+// description: Define the route for the authentication process of the admin login.
 Router::post('/admin/login/autentication', [DashBoardController::class, 'autentication'])->middleware([
     [AuthMiddleware::class, 'validateCSRFToken'],
     [AuthMiddleware::class, 'validateParamsLogin'],
     [AuthMiddleware::class, 'validateAttemps'],
 ]);
 
-/**
- * Define the route for the admin logout.
- * This route requires authentication.
- */
+// description: Define the route for the admin logout.
 Router::get('/admin/logout', [DashBoardController::class, 'logout'])->middleware([
     [AuthMiddleware::class, 'checkSession'],
 ]);
 
-/**
- * Define the route for the admin dashboard.
- * This route requires authentication and is a protected route.
- */
+// description: Define the route for the admin dashboard.
 Router::get('/admin/dashboard', [DashBoardController::class, 'dashboard'])->middleware([
     [AuthMiddleware::class, 'checkSession'],
 ]);
 
-/**
- * Define the route for the lost password page.
- * This route redirects if a session is already active.
- */
+// description: Define the route for the lost password page.
 Router::get('/admin/lost-password', [DashBoardController::class, 'lostPassword'])->middleware([
     [AuthMiddleware::class, 'redirectIfSession'],
 ]);
 
-/**
- * Define the route for sending the lost password email.
- * This route requires CSRF token validation and email validation.
- */
+// description: Define the route for sending the lost password email.
 Router::patch('/admin/lost-password/send', [DashBoardController::class, 'sendLostPassword'])->middleware([
     [AuthMiddleware::class, 'validateCSRFToken'],
     [AuthMiddleware::class, 'checkMail'],
 ]);
 
-/**
- * Define the route for the reset password page.
- */
+// description: Define the API route for the reset password page.
 Router::get('/admin/reset-password', [DashBoardController::class, 'resetPassword']);
 
-/**
- * Define the route for applying the password reset.
- * This route requires CSRF token validation and parameter validation.
- */
+// description: Define the API route for applying the password reset.
+// request-body name:new_password type:string description:Nueva contraseña
+// request-body name:comfirm_new_password type:string description:Confirmación de la nueva contraseña
+// request-body name:tokenReset type:string description:Token temporal para resetear la contraseña
+// request-body name:email type:int description:Email del usuario
 Router::post('/admin/login/apply-reset-password', [DashBoardController::class, 'applyChangePassword'])->middleware([
     [AuthMiddleware::class, 'validateCSRFToken'],
     [AuthMiddleware::class, 'validateParamsApplyResetPassword'],
 ]);
 
+// description: Define the route for reset password by user.
+// request-body name:id type:int description:ID del usuario
+// request-body name:password type:string description:Nueva contraseña
+// request-body name:password_confirmation type:string description:Confirmación de la nueva contraseña
 Router::post('/admin/login/resetPassByUser', [UsersController::class, 'resetPassByIdUser'])->middleware([
     [AuthMiddleware::class, 'validateCSRFToken'],
     [AuthMiddleware::class, 'checkSession'],
     [UsersMiddleware::class, 'validateParamsResetPassByUser'],
 ]);
 
+// description: Actualiza el avatar del usuario
+// request-body name:id type:int description:ID del usuario
+// request-body name:avatar type:file description:Archivo de imagen
 Router::post('/admin/usuarios/updateAvatar', [UsersController::class, 'updateAvatarById'])->middleware([
     [AuthMiddleware::class, 'validateCSRFToken'],
     [AuthMiddleware::class, 'checkSession'],
     [UsersMiddleware::class, 'validateParamsUpdateAvatar'],
 ]);
 
+// description: Define the route for the profile user page.
 Router::get('/admin/perfiluser', [DashBoardController::class, 'perfiluser'])->middleware([
     [AuthMiddleware::class, 'checkSession'],
 ]);
--- app/routes/E404Routes.php
+++ app/routes/E404Routes.php
@@ -7,4 +7,5 @@ namespace app\routes;
 use app\controllers\E404Controller;
 use versaWYS\kernel\Router;
 
+// description: Define the route for the 404 page.
 Router::get('/e404', [E404Controller::class, 'e404']);
