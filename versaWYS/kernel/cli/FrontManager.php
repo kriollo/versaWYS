9d60a29db2259b60e699e3334070063f374c6efb
kriollo <32275426+kriollo@users.noreply.github.com>
2025-01-17 23:44:31 -0300

    refactor: simplifica el script y elimina elementos innecesarios en FrontManager.php

--- versaWYS/kernel/cli/FrontManager.php
+++ versaWYS/kernel/cli/FrontManager.php
@@ -67,39 +67,14 @@ EOT;
         }
 
         $component = <<<'EOT'
-<script setup lang="ts">
+<script setup>
     import { ref } from 'vue';
-    import type { Ref } from 'vue';
     const title = ref('Hello World');
-    const count: Ref<number> = ref(0);
 </script>
 <template>
-    <div class="grid justify-center items-center content-center p-6">
+    <div class="grid justify-center items-center content-center">
         <h1 class="text-2xl font-bold clasePersonalizada">$moduleName</h1>
         <p class="clasePersonalizada">{{ title }}</p>
-    </div>
-    <div class="grid justify-center items-center content-center p-6">
-        <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            @click="title = 'Hello VersaWYS'">
-            Change Title
-        </button>
-        <div class="pt-6">
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                @click="count++">
-                +
-            </button>
-            <span
-                class="text-white font-bold py-2 px-4 rounded">
-                {{ count }}
-            </span>
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                @click="count--">
-                -
-            </button>
-        </div>
     </div>
 </template>
 <style scoped>
