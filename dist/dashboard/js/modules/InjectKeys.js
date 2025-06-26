b7ddbd3e79aeb1fe7f672da7c3f74e3acd0351a6
kriollo <kriollone@gmail.com>
2025-06-26 15:14:35 -0400

    Remove unused user management components and scripts from the dashboard, including user list, password update modal, and user profile functionalities. This cleanup enhances maintainability and reduces bundle size.

--- dist/dashboard/js/modules/InjectKeys.js
+++
@@ -1,5 +0,0 @@
-import { createInjection } from '@/dashboard/js/composables/injectStrict';
-const ShowModalSubFormInjection = createInjection('ShowModalSubForm');
-const ShowModalFormInjection = createInjection('ShowModalForm');
-export { ShowModalFormInjection, ShowModalSubFormInjection, };
-//# sourceMappingURL=InjectKeys.js.map
--- dist/dashboard/js/pinia-instancia.js
+++
@@ -1,5 +0,0 @@
-import { app } from '@/dashboard/js/vue-instancia';
-import { createPinia } from '/node_modules/pinia/dist/pinia.esm-browser.js';
-export const pinia = createPinia();
-app.use(pinia);
-//# sourceMappingURL=pinia-instancia.js.map
